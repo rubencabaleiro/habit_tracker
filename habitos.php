43c8ddb8ecca8bd16faa72234290f5e92b74a880
Ruben Comesaña Cabaleiro <cabaleiroruben16@gmail.com>
2019-01-17 09:50:55 +0100

    First commit

---
+++ habitos.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+	<title>Habitos</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+	
+	<?php
+		//Credenciais base de datos
+		$servername= "localhost";
+		$username= "alan";
+		$password= "turing";
+
+		//Crear conexion MySQL
+		$conn = mysqli_connect($servername, $username, $password, "ENIGMA");
+
+		//Comprobar conexion, se falla mostrar erro
+		if (!$conn) {
+			die('<p>Esto no esta funcionando: </p>' . mysqli_connect_error());
+			}
+		echo '<p>Conexion OK!</p>';
+		
+
+		//Coller 	
+
+		if (isset($_POST["nome"])){
+			$insert = "INSERT INTO Habitos (Nome) VALUES ('" . $_POST["nome"] . "');";
+			$result = mysqli_query($conn, $insert);
+			echo $result;
+			echo "<p>Habito " . $_POST['nome'] . " creado</p>";
+		}
+	
+	// Borrar habito (respuesta al GET con parametros
+	if (isset($_REQUEST["borrar"])) {
+		$delete = "DELETE FROM Habitos WHERE ID=" . $_REQUEST["borrar"] . ";";
+		$result = mysqli_query($conn, $delete);
+		echo $result;
+		echo "Habito borrado";
+	}
+
+	?>
+	<h1>Habitos</h1>
+	<?php
+		$lectura = "SELECT * FROM Habitos;";
+		$habitos = mysqli_query($conn, $lectura);
+		echo "Numero de habitos: " . mysqli_num_rows($habitos) . "<br>";
+		while($hab = mysqli_fetch_array($habitos)){
+			echo $hab['ID'] . " - " . $hab['Nome'] . " <a href=\"/habitos.php/?borrar=" . $hab['ID'] . "\">Borrar</a><br>";
+		}		
+		?>
+		<p>Se precisas axuda, le <a href="https://habitualmente.com/pasos-para-cambiar-de-habitos/">isto</a>.<p>
+		<form name="habito" method="post" action="/habitos.php">
+			<input type="text" id="nome" name="nome">
+			<button id="gardar" type="submit">Gardar</button>
+	</form>
+	
+</body>
+</html>
